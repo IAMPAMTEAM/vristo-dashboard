942295694bb2036f67ad1089aca20c35fcc53d27
yubeom <kimub1204@mz.co.kr>
2024-05-02 18:24:03 +0900

    Fix: 챗봇 knowledgebase 인식 못하는 버그 수정

--- src/pages/Chat/ChatContainer.tsx
+++ src/pages/Chat/ChatContainer.tsx
@@ -26,6 +26,15 @@ import IconCamera from '../../components/Icon/IconCamera';
 
 import OpenAI from 'openai';
 
+const instructions = `너는 Knowlege의 excel 파일을 읽어 대화 시 필요한 분석을 수행하고 차트를 생성해줘야해.
+Knowlege의 excel 파일 확장자는 .xlsx이며 너는 pandas를 내장 엔진으로 준비해서 이 파일을 분석을 해줘야해.
+먼저 sheet 이름 'metaSheet'를 읽어 'sheetName' 열에 나열된 sheet 이름에 대한  별명들을 'sheetNickNames'를 통해 파악해줘.
+그 다음 sheet 이름 'metaColumn'을 읽어 'columnName' 열에 나열된 각 열에 대한 별명들을 'columnNickNames'를 통해 파악해줘.
+만약 서비스에 대한 별칭으로 사용자가 질문을 한다면 sheet 이름 'metaServie'에 나열된 'serviceNickNames'를 통해 파악해줘.
+날짜에 대한 컬럼은 따로 없기 때문에 날짜에 대한 사용자의 질문이 들어오면 YYYY.MM.DD 형식의 셀을 날짜로 파악해줘.
+이제부터 너는 우리 대화에서 언급될 각 단어들을 이해할때 위에서 언급된 단어들과 유사한지 고려해 적절한 답변을 해줘야해
+넌 답변할 때 중간 답변은 주지 말고 최종 답변만 해야해. 그리고 답변은 모두 한국어로 해야해.`;
+
 const openai = new OpenAI({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
@@ -87,13 +96,26 @@ const ChatContainer = () => {
 
     useEffect(() => {
         const setupThread = async () => {
-            const assistants = await openai.beta.assistants.list();
-            const assistant = assistants.data.find((assistant) => (assistant.name = '유범 봇 4호기'));
+            const assistant = await openai.beta.assistants.create({
+                instructions,
+                name: '유범봇 5호기',
+                tools: [
+                    {
+                        type: 'code_interpreter',
+                    },
+                ],
+                model: 'gpt-4-turbo',
+                tool_resources: {
+                    code_interpreter: {
+                        file_ids: ['file-RyYExNGdlLpfly8DkDvnebp9'],
+                    },
+                },
+            });
 
             if (!assistant) {
                 return;
             }
-
+            console.log(assistant);
             const { id: assistantId } = assistant;
             const { id: threadId } = await openai.beta.threads.create();
 
@@ -105,6 +127,8 @@ const ChatContainer = () => {
     }, []);
 
     useEffect(() => {
+        console.log(assistantId, threadId, runId);
+
         if (!assistantId || !threadId || !runId) {
             return;
         }
@@ -114,20 +138,6 @@ const ChatContainer = () => {
 
             if (runStatus.status === 'completed') {
                 const messages = await openai.beta.threads.messages.list(threadId);
-
-                const botInfo = contactList.find((d) => d.userId === selectedUser.userId) as {
-                    userId: number;
-                    name: string;
-                    path: string;
-                    time: string;
-                    preview: string;
-                    messages: {
-                        fromUserId: number;
-                        toUserId: number;
-                        text: string;
-                    }[];
-                    active: boolean;
-                };
 
                 const message = messages.data[0].content[0] as { text: { value: string; annotations: any[] }; type: string };
                 console.log(contactList[0].messages);
