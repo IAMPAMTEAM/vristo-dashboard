d2faec5b7c847a7ddc2808acd03b0ef99417ae05
이승연 <sylee1274@mz.co.kr>
2024-05-08 17:32:32 +0900

    Chore: Design retouch

--- src/pages/Entry/EntryContainer.tsx
+++ src/pages/Entry/EntryContainer.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
 import EntryLogin from './_partials/EntryLogin';
 import EntryMenu from './_partials/EntryMenu';
 import { useSelector } from 'react-redux';
+import LogoHybrix from '@/assets/icons/LogoHybrix.svg';
 
 const EntryContainer = () => {
   const user = useSelector((state: any) => state['userReducer']['value']);
@@ -9,9 +9,9 @@ const EntryContainer = () => {
   return (
     <div>
       <div className='absolute inset-0 bg-gradient-to-r from-[#6667AB] to-[#ED8C00]'>{/* <img src="/assets/images/auth/bg-gradient.png" alt="image" className="h-full w-full object-fill" /> */}</div>
-      <div className='absolute p-2 text-2xl font-bold mb-4'>
-        {/* logo */}
-        IAMPAM
+      <div className='absolute p-2 text-2xl font-bold flex gap-3'>
+        <img className='w-8 ltr:-ml-1 rtl:-mr-1 inline' src={LogoHybrix} alt='logo' />
+        <p>IAMPAM</p>
       </div>
 
       <div className='relative flex min-h-screen items-center justify-center  sm:px-16 pt-12'>{user.email && user.password ? <EntryMenu /> : <EntryLogin />}</div>
